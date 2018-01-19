634f09aa42c13911643dd2161f4f4691286a1e92
Filipe Fernandes <me@filipesf.com>
2018-01-19 22:31:22 +0000

    ES5

--- 01-design-markup/main.js
+++ 01-design-markup/main.js
@@ -1,29 +1,30 @@
-const card  = document.querySelector('#js-card')
-const flipB = document.querySelector('#js-flip-back')
-const flipF = document.querySelector('#js-flip-front')
+'use strict';
+
+var card = document.querySelector('#js-card');
+var flipB = document.querySelector('#js-flip-back');
+var flipF = document.querySelector('#js-flip-front');
 
 
 
 /**
  * Flip Card
  */
-const flipCard = e => {
-  card.classList.toggle('is-active')
-  e.preventDefault()
-}
-flipB.addEventListener('click', flipCard)
-flipF.addEventListener('click', flipCard)
+var flipCard = function flipCard(e) {
+  card.classList.toggle('is-active');
+  e.preventDefault();
+};
+flipB.addEventListener('click', flipCard);
+flipF.addEventListener('click', flipCard);
 
 
 
 /**
  * Adjusting Card height
  */
-const cardF = document.querySelector('#js-card-front')
-const initCardHeight = card.style.height = `${cardF.offsetHeight}px`
-window.addEventListener('resize', () =>
-  card.offsetHeight < cardF.offsetHeight
-    ? card.style.height = `${cardF.offsetHeight}px`
-    : card.style.height = initCardHeight
-)
-
+var cardF = document.querySelector('#js-card-front');
+var initCardHeight = card.style.height = cardF.offsetHeight + 'px';
+window.addEventListener('resize', function () {
+  return card.offsetHeight < cardF.offsetHeight
+    ? card.style.height = cardF.offsetHeight + 'px'
+    : card.style.height = initCardHeight;
+});
